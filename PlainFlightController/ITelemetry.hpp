bf362b59a4358a1369bde46a8bfb9674353e7788
Cyberslug <52554706+Cyberslug@users.noreply.github.com>
2026-06-21 13:14:35 +0800

    add stragglers

--- PlainFlightController/ITelemetry.hpp
+++ PlainFlightController/ITelemetry.hpp
@@ -1,7 +1,7 @@
 /* 
 * Original File Author: D. Gamble (Github: Cyberslug)
 *
-* Copyright (c) 2025 P.Cook (alias 'plainFlight')
+* Copyright (c) 2026 P.Cook (alias 'plainFlight')
 *
 * This file is part of the PlainFlightController distribution (https://github.com/plainFlight/plainFlightController).
 * 
